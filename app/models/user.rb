df638cc14d6075c714802af8865cdebc85e82224
Sam Gergi <samgergi@hotmail.com>
2017-07-20 15:30:37 +0200

    Tests install fix and User model test working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-    # validates :full_name, presence: true
-    # validates :batch, presence: true
+    validates :full_name, presence: true
+    validates :batch, presence: true
     validates :email, presence: true
-    # validates :student, presence: true
+#    validates :student, presence: true
 
 
 
