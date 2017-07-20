c689344d43cf75730393b6444c1277dbadd2dbd0
Sam Gergi <samgergi@hotmail.com>
2017-07-20 15:57:55 +0200

    User model tests complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:full_name)}
+    it { is_expected.to validate_presence_of(:batch) }
+
   end
 end
